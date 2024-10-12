70e0138ce71d1ccf02291ace4c63469f385c1465
Simon Boudrias <admin@simonboudrias.com>
2024-10-12 15:50:28 -0400

    Bootstrap: set as esm module

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/cocktails",
-    reactStrictMode: true,
+  output: 'export',
+  basePath: '/cocktails',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
