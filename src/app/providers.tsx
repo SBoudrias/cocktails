862541451a552fcc4a9db6e86e15611531a2b27d
Simon Boudrias <admin@simonboudrias.com>
2024-10-12 17:58:42 -0400

    Remove next UI

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,7 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { NextUIProvider } from '@nextui-org/react';
 import { ConfigProvider } from 'antd-mobile';
 import enUS from 'antd-mobile/es/locales/en-US';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  return (
-    <ConfigProvider locale={enUS}>
-      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
-    </ConfigProvider>
-  );
+  return <ConfigProvider locale={enUS}>{children}</ConfigProvider>;
 }
