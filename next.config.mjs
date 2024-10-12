954772c8c74fa1a65233d2ca9707df24dceeb60a
Simon Boudrias <admin@simonboudrias.com>
2024-10-12 15:24:21 -0400

    Config: next config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/cocktails",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
