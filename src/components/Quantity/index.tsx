98044f6ff87aa5593c836eb48ebd74a65716e91b
Simon Boudrias <admin@simonboudrias.com>
2024-10-13 12:43:43 -0400

    Feat: Add extra fractions rendering

--- src/components/Quantity/index.tsx
+++ src/components/Quantity/index.tsx
@@ -3,15 +3,30 @@ import styles from './style.module.css';
 import { Unit } from '@/types/Ingredient';
 
 const displayFraction: Record<number, string> = {
+  0.125: '⅛',
+  0.12: '⅛',
   0.25: '¼',
+  0.3: '⅓',
+  0.33: '⅓',
   0.5: '½',
+  0.66: '⅔',
+  0.67: '⅔',
   0.75: '¾',
+};
+
+const unitType: Record<Unit, 'imperial' | 'metric' | 'other'> = {
+  oz: 'imperial',
+  tsp: 'imperial',
+  tbsp: 'imperial',
+  ml: 'metric',
+  dash: 'other',
+  drop: 'other',
 };
 
 export default function Quantity({ amount, unit }: { amount: number; unit: Unit }) {
   let displayAmount: number | string = amount;
   // Display with a fraction
-  if (unit === 'oz' && amount % 1 !== 0) {
+  if (unitType[unit] === 'imperial' && amount % 1 !== 0) {
     const base = Math.floor(amount);
     const fraction = amount - base;
     if (fraction in displayFraction) {
--- src/types/Ingredient.ts
+++ src/types/Ingredient.ts
@@ -1,5 +1,5 @@
 export type IngredientType = 'spirit' | 'juice' | 'syrup' | 'liqueur';
-export type Unit = 'oz' | 'ml' | 'dash';
+export type Unit = 'oz' | 'ml' | 'dash' | 'tsp' | 'tbsp' | 'drop';
 
 export type Ingredient = {
   name: string;
