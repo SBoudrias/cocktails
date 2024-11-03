68a514f73e97157dc839fb19dcf021872bc23aa0
Simon Boudrias <admin@simonboudrias.com>
2024-11-03 13:44:43 -0500

    MUI: App footer

--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -1,18 +1,38 @@
 'use client';
 
-import { Footer } from 'antd-mobile';
+import { Link, Stack, Typography } from '@mui/material';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 export default function AppFooter() {
   return (
-    <Footer
-      content="Cocktail Index"
-      links={[
-        {
-          text: 'Contribute on GitHub',
-          href: 'https://github.com/SBoudrias/cocktails/',
-        },
-      ]}
-      style={{ marginTop: '24px' }}
-    />
+    <Stack spacing={2} sx={{ py: 2 }}>
+      <Stack
+        direction="row"
+        divider
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Link
+          href="https://github.com/SBoudrias/cocktails/"
+          underline="none"
+          target="_blank"
+          rel="noopener"
+        >
+          Contribute on GitHub <ArrowOutwardIcon sx={{ fontSize: 'medium' }} />
+        </Link>
+      </Stack>
+      <Stack
+        direction="row"
+        divider
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography color="textSecondary">Cocktail Index</Typography>
+      </Stack>
+    </Stack>
   );
 }
