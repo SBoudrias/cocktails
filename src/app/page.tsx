5f261bfc01341dc5084789ef84cc46e63349f5a7
Simon Boudrias <admin@simonboudrias.com>
2024-11-09 15:03:15 -0500

    Fix: required suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import Search from './SearchBar';
 import { getAllData } from '@/modules/entities';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Cocktail Index | Search',
@@ -11,7 +12,9 @@ export default async function SearchPage() {
 
   return (
     <>
-      <Search recipes={recipes} />
+      <Suspense>
+        <Search recipes={recipes} />
+      </Suspense>
     </>
   );
 }
