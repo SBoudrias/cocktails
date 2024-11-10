7c9f92f1ea8916cee4e120503344d0667654f1b9
Simon Boudrias <admin@simonboudrias.com>
2024-11-10 13:47:27 -0500

    Feat: Add sitemaps & robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://sboudrias.github.io/cocktails',
+  generateRobotsTxt: true,
+  outDir: 'src/public',
+};
+
+export default config;
