b484525578a37879c6af748bb905f4a48cdbab90
Simon Boudrias <admin@simonboudrias.com>
2025-02-01 16:34:53 -0500

    Feat: Add link to the recipe files. Fix #12

--- src/app/recipes/[type]/[source]/[recipe]/page.tsx
+++ src/app/recipes/[type]/[source]/[recipe]/page.tsx
@@ -4,7 +4,8 @@ import RecipeDetails from '@/components/RecipeDetails';
 import { getRecipe } from '@/modules/recipes';
 import { getRecipePageParams } from '@/modules/params';
 import RecipeSources from '@/components/RecipeSources';
-import { Box } from '@mui/material';
+import { Box, Button, Card, CardContent, Stack } from '@mui/material';
+import BugReportIcon from '@mui/icons-material/BugReport';
 import { Source } from '@/types/Source';
 
 type Params = { type: Source['type']; source: string; recipe: string };
@@ -31,12 +32,25 @@ export default async function RecipePage({ params }: { params: Promise<Params> }
   const { type, source, recipe: recipeSlug } = await params;
   const recipe = await getRecipe({ type: type, slug: source }, recipeSlug);
 
+  const githubDataUrl = `https://github.com/SBoudrias/cocktails/edit/main/src/data/recipes/${type}/${source}/${recipeSlug}.json`;
+
   return (
     <>
       <AppHeader title={recipe.name} />
       <Box component="main">
         <RecipeDetails recipe={recipe} />
         <RecipeSources recipe={recipe} />
+        <Card sx={{ m: 2 }}>
+          <CardContent>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <BugReportIcon />
+              <span>Found an error?</span>
+              <Button href={githubDataUrl} target="_blank" rel="noopener nofollow">
+                Fix it over here!
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
       </Box>
     </>
   );
