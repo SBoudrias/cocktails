abd1bb7b69dc09755a50e96deb6219b39752bbd6
Simon Boudrias <admin@simonboudrias.com>
2025-02-15 15:04:32 -0500

    Feat: Add acid-adjusting calculators to juice pages

---
+++ src/components/AcidAdjustingCalculator/index.tsx
@@ -0,0 +1,173 @@
+'use client';
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  TextField,
+  ToggleButton,
+  ToggleButtonGroup,
+} from '@mui/material';
+import { useState } from 'react';
+import Video from '@/components/Video';
+
+const LIME_ACIDITY = 6;
+
+function ClassicalCalculator({ defaultAcidity }: { defaultAcidity: number }) {
+  const [targetAcidityValue, setTargetAcidity] = useState<string | number>(LIME_ACIDITY);
+  const [acidAmountValue, setAcidAmount] = useState<string | number>(defaultAcidity);
+  const [weightValue, setWeight] = useState<string | number>(100);
+
+  const targetAcidity = parseFloat(targetAcidityValue as string);
+  const acidAmount = parseFloat(acidAmountValue as string);
+  const weight = parseFloat(weightValue as string);
+
+  // 2/3 citric, 1/3 malic
+  // We're assuming the acid content of the juice is only citric which is why we remove it from
+  // our citric target. This is almost always the case, warranting this slight simplification.
+  const targetCitric = targetAcidity * (2 / 3) - acidAmount;
+  const targetMalic = targetAcidity * (1 / 3);
+
+  const citricAcid = (targetCitric * weight) / 100;
+  const malicAcid = (targetMalic * weight) / 100;
+
+  return (
+    <>
+      <CardContent>
+        <Stack spacing={2}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <TextField
+              label="Target %"
+              value={targetAcidityValue}
+              type="number"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setTargetAcidity(event.target.value);
+              }}
+            />
+            <TextField
+              label="Juice %"
+              value={acidAmountValue}
+              type="number"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setAcidAmount(event.target.value);
+              }}
+            />
+            <TextField
+              label="Juice weight (grams)"
+              value={weightValue}
+              type="number"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setWeight(event.target.value);
+              }}
+            />
+          </Stack>
+          <span>Mix & dilute into juice:</span>
+          <TextField
+            label="Citric Acid"
+            value={isNaN(citricAcid) ? '' : `${citricAcid} grams`}
+            slotProps={{
+              input: {
+                readOnly: true,
+              },
+            }}
+          />
+          <TextField
+            label="Malic Acid"
+            value={isNaN(malicAcid) ? '' : `${malicAcid} grams`}
+            slotProps={{
+              input: {
+                readOnly: true,
+              },
+            }}
+          />
+        </Stack>
+      </CardContent>
+    </>
+  );
+}
+
+function AcidAdjusterCalculator({ defaultAcidity }: { defaultAcidity: number }) {
+  const [targetAcidityValue, setTargetAcidity] = useState<string | number>(LIME_ACIDITY);
+  const [acidAmountValue, setAcidAmount] = useState<string | number>(defaultAcidity);
+
+  const targetAcidity = parseFloat(targetAcidityValue as string);
+  const acidAmount = parseFloat(acidAmountValue as string);
+  const diff = targetAcidity - acidAmount;
+
+  return (
+    <>
+      <CardContent>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <TextField
+            label="Target %"
+            value={targetAcidityValue}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTargetAcidity(event.target.value);
+            }}
+          />
+          <span>-</span>
+          <TextField
+            label="Juice %"
+            value={acidAmountValue}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setAcidAmount(event.target.value);
+            }}
+          />
+          <span>=</span>
+          <TextField
+            value={isNaN(diff) ? '' : `${diff}ml`}
+            slotProps={{
+              input: {
+                readOnly: true,
+              },
+            }}
+          />
+        </Stack>
+      </CardContent>
+      <CardContent>
+        <Video id="8pFBK0KWkNc" />
+      </CardContent>
+    </>
+  );
+}
+
+export default function AcidAdjustingCalculator({
+  defaultAcidity,
+}: {
+  defaultAcidity: number;
+}) {
+  const [type, setType] = useState<'lime' | 'acid-adjuster'>('acid-adjuster');
+
+  return (
+    <Card>
+      <CardHeader
+        title="Acid Adjusting"
+        action={
+          <ToggleButtonGroup
+            value={type}
+            onChange={(_, newVal) =>
+              setType(newVal === 'lime' ? 'lime' : 'acid-adjuster')
+            }
+            size="small"
+            aria-label="Preferred acid-adjusting technique"
+            exclusive
+          >
+            <ToggleButton value="acid-adjuster">Acid Adjuster</ToggleButton>
+            <ToggleButton value="lime">Lime</ToggleButton>
+          </ToggleButtonGroup>
+        }
+        sx={{
+          pb: 3,
+        }}
+      />
+      {type === 'lime' ? (
+        <ClassicalCalculator defaultAcidity={defaultAcidity} />
+      ) : (
+        <AcidAdjusterCalculator defaultAcidity={defaultAcidity} />
+      )}
+    </Card>
+  );
+}
