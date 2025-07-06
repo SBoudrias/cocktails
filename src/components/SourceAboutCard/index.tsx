90d833d66ffa680f57010f0e618cea569041df63
Simon Boudrias <admin@simonboudrias.com>
2025-07-06 14:27:08 -0400

    fix(#26): Missing page # in recipes

--- src/components/SourceAboutCard/index.tsx
+++ src/components/SourceAboutCard/index.tsx
@@ -4,6 +4,7 @@ import { type SxProps } from '@mui/material';
 import BookAboutCard from './Book';
 import YoutubeAboutCard from './Youtube';
 import { match } from 'ts-pattern';
+import slugify from '@sindresorhus/slugify';
 
 export default function SourceAboutCard({
   source,
@@ -17,7 +18,8 @@ export default function SourceAboutCard({
   return match(source)
     .with({ type: 'book' }, (source) => {
       const ref = refs.find(
-        (ref): ref is BookRef => ref.type === 'book' && ref.title === source.slug,
+        (ref): ref is BookRef =>
+          ref.type === 'book' && slugify(ref.title) === source.slug,
       );
       return <BookAboutCard source={source} page={ref?.page} sx={sx} />;
     })
