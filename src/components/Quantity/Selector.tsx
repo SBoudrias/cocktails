ac758f230ac3a5bcd4bbd68c5a3edb0725b87e45
Simon Boudrias <admin@simonboudrias.com>
2025-08-10 15:07:30 -0400

    fixup: design of the serving input

--- src/components/Quantity/Selector.tsx
+++ src/components/Quantity/Selector.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ToggleButtonGroup, ToggleButton, Toolbar } from '@mui/material';
+import { ToggleButtonGroup, ToggleButton } from '@mui/material';
 
 export type Unit = 'oz' | 'ml';
 
@@ -12,17 +12,15 @@ export default function UnitSelector({
   onChange: (unit: Unit) => void;
 }) {
   return (
-    <Toolbar>
-      <ToggleButtonGroup
-        value={value}
-        onChange={(_, newVal) => onChange(newVal === 'oz' ? 'oz' : 'ml')}
-        size="small"
-        aria-label="Preferred measurement unit"
-        exclusive
-      >
-        <ToggleButton value="oz">oz</ToggleButton>
-        <ToggleButton value="ml">ml</ToggleButton>
-      </ToggleButtonGroup>
-    </Toolbar>
+    <ToggleButtonGroup
+      value={value}
+      onChange={(_, newVal) => onChange(newVal === 'oz' ? 'oz' : 'ml')}
+      size="small"
+      aria-label="Preferred measurement unit"
+      exclusive
+    >
+      <ToggleButton value="oz">oz</ToggleButton>
+      <ToggleButton value="ml">ml</ToggleButton>
+    </ToggleButtonGroup>
   );
 }
