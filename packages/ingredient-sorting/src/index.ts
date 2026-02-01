e554b7fb8599ef8a47299073f1b2a57fb32c7490
Simon Boudrias <simon.boudrias@datadoghq.com>
2026-02-01 11:46:12 -0500

    refactor: remove barrel file, export directly from source

--- packages/ingredient-sorting/src/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  type SortableIngredient,
-  UNIT_PRIORITIES,
-  INGREDIENT_PRIORITIES,
-  APPLICATION_PRIORITIES,
-  convertQuantityToMl,
-  getApplicationTechniquePriority,
-  compareIngredients,
-} from './ingredientSorting.ts';
